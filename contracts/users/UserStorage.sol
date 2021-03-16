fe7a8ec70f167978ff983176ffd6c5ed368390bd
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-03-16 14:37:58 +0800

    master Create user storage contract and write a js test

---
+++ contracts/users/UserStorage.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.2;
+
+contract UserStorage {
+
+  mapping(uint => Profile) profiles;
+
+  struct Profile {
+    uint id;
+    bytes32 username;
+  }
+
+  uint latestUserId = 0;
+
+  function createUser(bytes32 _username) public returns(uint) {
+    latestUserId++;
+    profiles[latestUserId] = Profile(latestUserId, _username);
+    return latestUserId;
+  }
+
+}
---
+++ test/integration/users.js
@@ -0,0 +1,10 @@
+const UserStorage = artifacts.require('UserStorage');
+
+contract('users', () => {
+  it('can create user', async () => {
+    const storage = await UserStorage.deployed();
+    const username = web3.utils.fromAscii('adam');
+    const tx = await storage.createUser(username);
+    assert.isOk(tx);
+  })
+})
