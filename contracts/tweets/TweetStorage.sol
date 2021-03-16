e2cddff1a487ffd88545aa8c9f308f5eff5fdafa
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-03-16 15:41:43 +0800

    master Create tweet storage contract with createTweet method and accompanying test

---
+++ contracts/tweets/TweetStorage.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.8.2;
+
+contract TweetStorage {
+  mapping(uint => Tweet) public tweets;
+  
+  struct Tweet {
+    uint id;
+    string text;
+    uint userId;
+    uint postedAt;
+  }
+
+  uint latestTweetId = 0;
+
+  function createTweet(uint _userId, string memory _text) public returns(uint) {
+    latestTweetId++;
+
+    tweets[latestTweetId] = Tweet(latestTweetId, _text, _userId, block.timestamp);
+
+    return latestTweetId;
+  }
+
+}
