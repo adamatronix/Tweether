d4ca6df19f7f90764ffc95add4fab9d46abc42a8
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-03-17 17:09:49 +0800

    metamask Find a way of interacting with contracts

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { eth } from '../web3/provider';
+import { eth, getInstance } from '../web3/provider';
+import UserStorage from "../web3/artifacts/UserStorage.json";
 
 export default class IndexPage extends React.Component {
 
@@ -7,13 +8,15 @@ export default class IndexPage extends React.Component {
     try {
       await ethereum.enable()
       const addresses = await eth.getAccounts()
-      console.log(addresses);
-
       const balance = await eth.getBalance(addresses[0]);
-      console.log(balance);
     } catch (err) {
       console.error("User denied access to their ETH addresses!")
     }
+
+    const storage = await getInstance(UserStorage)
+    const { username } = await storage.profiles.call(1)
+    console.log("Got username:", username);
+
   }
 
   render() {
