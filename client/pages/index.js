1ebfa3205f199b2f99311d7b77eeb2fac38fde3a
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-03-17 15:27:15 +0800

    react-next Run some metamask integration stuff

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,21 @@
 import React from 'react'
+import { eth } from '../web3/provider';
 
 export default class IndexPage extends React.Component {
+
+  async componentDidMount() {
+    try {
+      await ethereum.enable()
+      const addresses = await eth.getAccounts()
+      console.log(addresses);
+
+      const balance = await eth.getBalance(addresses[0]);
+      console.log(balance);
+    } catch (err) {
+      console.error("User denied access to their ETH addresses!")
+    }
+  }
+
   render() {
     return (
       <h1>hello world!</h1>
